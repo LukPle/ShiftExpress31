31141adb801a99f8373d6f4c7574d7c2ff36ccf2
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2023-11-05 18:25:34 +0100

    Added Footer/Header and basic structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,15 @@ import type { AppProps } from 'next/app'
 import { CssVarsProvider } from '@mui/joy/styles';
 import '@fontsource/inter';
 import theme from '../utils/theme';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CssVarsProvider theme={theme}>
-        <Component {...pageProps} />
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
     </CssVarsProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
-import { Typography } from "@mui/joy"
+import { Typography, Stack } from "@mui/joy"
+import indexStyles from '../styles/index.module.css';
 
 export default function Home() {
   return (
-    <>
-    <Typography>Hello World!</Typography>
-    </>
+    <Stack direction={"row"}>
+      <Stack direction={"column"} my={10}>
+      <div className={indexStyles.ring}></div>
+      <div className={indexStyles.colorLine} style={{backgroundColor: "#56B9E3"}}></div>
+      <div className={indexStyles.ring}></div>
+      <div className={indexStyles.colorLine} style={{backgroundColor: "#84BE55"}}></div>
+      <div className={indexStyles.ring}></div>
+      <div className={indexStyles.colorLine} style={{backgroundColor: "#8A237E"}}></div>
+      <div className={indexStyles.ring}></div>
+      <div className={indexStyles.colorLine} style={{backgroundColor: "#D12D26"}}></div>
+      </Stack>
+      <Stack direction={"column"} p={10} alignItems="center" width="100%">
+        <Typography level="h2">Here could be our code!</Typography>
+      </Stack>
+    </Stack>
   )
 }
