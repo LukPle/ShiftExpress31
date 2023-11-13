9686ae7aeac76855dc75bdcc4845d44d9be07da8
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2023-11-13 19:48:10 +0100

    Implemented base theme

--- src/utils/themeTypes.ts
+++ src/utils/themeTypes.ts
@@ -1,15 +1,33 @@
 import { ColorSchemes } from '@mui/joy/styles';
 
 declare module '@mui/joy/styles' {
-  interface PaletteBackground {
-    default: string;
+  interface Palette {
+    primary: {
+      50: string;
+      100: string;
+      200: string;
+      300: string;
+      400: string;
+      500: string;
+      600: string;
+      700: string;
+      800: string;
+      900: string;
+    };
+    background: {
+      surface: string;
+      body: string;
+    };
+    text: {
+      primary: string;
+      secondary: string;
+      tertiary: string;
+    };
   }
 
   interface ColorSchemes {
     light: {
-      palette: {
-        background: PaletteBackground;
-      };
+      palette: Palette;
     };
   }
 }
