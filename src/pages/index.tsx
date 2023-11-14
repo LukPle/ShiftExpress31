392c3a734860edcb78ef1a93df68ab380083aff0
MxnWgd <maxiw@outlook.com>
2023-11-14 21:45:03 +0100

    Revert due to mistake commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Card, Typography, Stack, Box } from "@mui/joy"
+import { Card, Typography, Stack } from "@mui/joy"
 import indexStyles from '../styles/index.module.css';
 import Keyfact from "@/components/Keyfact";
 import Map from "@/components/Map";
@@ -20,20 +20,19 @@ export default function Home() {
     <Stack direction={"column"} mx={17} my={7}>
 
       <Stack direction="column">
-        <Box></Box>
         <Typography level="h1">Visualizing the transportation <br/> shift in Germany</Typography>
         <Typography mt={2}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</Typography>
       </Stack>
 
 
-      <Stack direction={"column"} id="project" mt={7} className={indexStyles.lineLeftStack}>
-        <Typography level="h2" className={indexStyles.markerLeftHeading}>Project</Typography>
+      <Stack direction={"column"} id="project" mt={7}>
+        <Typography level="h2">Project</Typography>
         <Typography mt={2}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo do</Typography>
       </Stack>
 
 
-      <Stack direction={"column"} id="insights" mt={7} className={indexStyles.lineLeftStack}>
-        <Typography level="h2" className={indexStyles.markerLeftHeading}>Insights and Map</Typography>
+      <Stack direction={"column"} id="insights" mt={7}>
+        <Typography level="h2">Insights and Map</Typography>
         <Stack direction={"column"}>
           <Typography level="h3" mt={4}>Our Key Findings</Typography>
           <Stack
@@ -63,18 +62,18 @@ export default function Home() {
       </Stack>
 
       
-      <Stack direction={"column"} mt={7} className={indexStyles.lineLeftHalfAcrossStack}>
-        <Typography level="h2" id="team" className={indexStyles.markerLeftHeading}>Team</Typography>
-        <Card
-          variant="outlined"
-          sx={{
-            width: 800,
-            height: 200,
-            mt: 2
-          }}
-        >
-        </Card>
-      </Stack>
+        <Stack direction={"column"} mt={7}>
+          <Typography level="h2" id="team">Team</Typography>
+          <Card
+            variant="outlined"
+            sx={{
+              width: 800,
+              height: 200,
+              mt: 2
+            }}
+          >
+          </Card>
+        </Stack>
     </Stack>
 
   )
