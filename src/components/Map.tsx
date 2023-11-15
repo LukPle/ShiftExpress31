887648202b5ed4acc2ef8191a5af9f404be9b636
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2023-11-15 22:34:39 +0100

    Added basic interactivity to map
    OnHover State highlighting

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,24 +1,37 @@
 import { Card } from "@mui/joy";
 import * as d3 from "d3";
 import germanyGeoJSON from "../data/germany-states.json";
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { FeatureCollection } from 'geojson';
 
 const mapData: FeatureCollection = germanyGeoJSON as FeatureCollection;
 
 const MapChart: React.FC = () => {
     const svgRef = useRef<SVGSVGElement | null>(null);
+    const [selectedState, setSelectedState] = useState(null);
 
     useEffect(() => {
         if (!svgRef.current) return;
 
         const svg = d3.select(svgRef.current);
 
-        // Define projection (you may need to adjust the parameters)
+        // Define projection
         const projection = d3.geoMercator().fitSize([width, height], mapData);
 
         // Create a path generator
         const pathGenerator = d3.geoPath().projection(projection);
+
+        //Handels the mouse hover
+        const handleMouseOver = (event: React.MouseEvent<SVGPathElement, MouseEvent>, d: any) => {
+            setSelectedState(d.properties.name);
+            d3.select(event.currentTarget as Element).style('fill', 'lightblue');
+        };
+        
+        //Handels the mouse exit
+        const handleMouseOut = (event: React.MouseEvent<SVGPathElement, MouseEvent>, d: any) => {
+            setSelectedState(null);
+            d3.select(event.currentTarget as Element).style('fill', 'blue');
+        };        
 
         // Render the map
         svg
@@ -27,20 +40,27 @@ const MapChart: React.FC = () => {
             .enter()
             .append('path')
             .attr('d', (d) => pathGenerator(d) as string)
-            .style('fill', 'blue'); // Customize fill color as needed
+            .style('fill', 'blue')
+            .on('mouseover', handleMouseOver)
+            .on('mouseout', handleMouseOut);
 
     }, []);
 
-    const width = 500; // Set your desired width
-    const height = 600; // Set your desired height
+    const width = 500;
+    const height = 600;
 
     return (
-        <Card variant="outlined" sx={{width: 500, height: 600}}>
+        <Card variant="outlined" sx={{ width: 500, height: 600 }}>
             <svg
                 ref={svgRef}
                 width={width}
                 height={height}
             ></svg>
+            {selectedState && (
+                <div style={{ position: 'absolute', pointerEvents: 'none' }}>
+                    {selectedState}
+                </div>
+            )}
         </Card>
     );
 };
