3808cd0dd7ca037d2d9e7b25c821f19493a07233
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2023-11-25 11:43:15 +0100

    Fixing typescript error in TeamTile

--- src/components/TeamTile.tsx
+++ src/components/TeamTile.tsx
@@ -1,19 +1,28 @@
-import { Card, Typography } from "@mui/joy"
+import { Card, Typography } from "@mui/joy";
 import Image from 'next/image';
 import teamTileStyles from '../styles/teamTiles.module.css';
 
-export default function TeamTile({className, name, imageSrc, desc}) {
+// Define a type for the props
+type TeamTileProps = {
+  className: string;
+  name: string;
+  imageSrc: string;
+  desc: string;
+};
+
+// Use the type for the component's props
+export default function TeamTile({ className, name, imageSrc, desc }: TeamTileProps) {
     return (
       <Card
           className={teamTileStyles.teamTile + ' ' + className} 
           sx={{
-          width: 300,
-          height: 200
-      }}
+            width: 300,
+            height: 200
+          }}
       >
-        <Image src={imageSrc} alt={name} className={teamTileStyles.teamTileImage}/>
+        <Image src={imageSrc} alt={name} className={teamTileStyles.teamTileImage} layout='fill'/>
         <Typography level="h2" className={teamTileStyles.teamTileTitle}>{name}</Typography>
         <Typography mt={1} className={teamTileStyles.teamTileDesc}>{desc}</Typography>
       </Card>
-    )
+    );
 }
