6d70001d27013442ebcdfe629c3daedfdacd74ad
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2023-12-09 15:26:20 +0100

    Implemented basis for key findings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ChapterArea from '@/components/ChapterArea';
-import { Card, Typography, Stack, AccordionGroup, Accordion, AccordionDetails, AccordionSummary } from "@mui/joy";
-import {Train, DirectionsCar, MergeType } from '@mui/icons-material';
+import { Card, CardContent, CardCover, Typography, Stack, AccordionGroup, Accordion, AccordionDetails, AccordionSummary, Button } from "@mui/joy";
+import { Train, DirectionsCar, MergeType, TrendingUp, Coronavirus } from '@mui/icons-material';
 import indexStyles from '../styles/index.module.css';
 import Keyfact from "@/components/Keyfact";
 import Map from "@/components/Map";
@@ -21,13 +21,21 @@ import BarChartCombined from "@/components/ChartsCombined/BarChartCombined";
 import pTData from "../data/pT.json";
 import carData from "../data/car.json";
 import popData from "../data/population.json";
+import { max } from 'd3';
 
 type HomeProps = {
   currentSection: number;
   setSection: (section: number) => void;
 };
 
+enum KeyFinding {
+  None = 'NONE',
+  Shift = 'SHIFT',
+  Covid = 'COVID',
+}
+
 const Home: React.FC<HomeProps> = ({ currentSection, setSection }) => {
+  const [currentKeyFinding, setCurrentKeyFinding] = useState<KeyFinding>(KeyFinding.None);
   const sectionOffsets: number[] = [];
   const introSectionRef = useRef<HTMLDivElement>(null);
   const projectSectionRef = useRef<HTMLDivElement>(null);
@@ -55,29 +63,41 @@ const Home: React.FC<HomeProps> = ({ currentSection, setSection }) => {
       window.removeEventListener('scroll', scrollHandleSection);
     };
   }, []);
-  
+
+  const renderKeyFinding = () => {
+    switch (currentKeyFinding) {
+      case KeyFinding.Shift:
+        return <div>Shift Component</div>;
+      case KeyFinding.Covid:
+        return <div>Covid Component</div>;
+      case KeyFinding.None:
+      default:
+        return <div>Select a key finding to see more details.</div>;
+    }
+  }
+
 
   return (
     <>
       <Stack direction={"column"} mx={13} my={7}>
-          <ChapterArea>
-            <Stack direction="column" className={indexStyles.headingStack} ref={introSectionRef}>
-              {/* SVG with line background */}
-              <svg className={indexStyles.headingSvg} viewBox="0 0 1000 200" version='1.1' xmlns='http://www.w3.org/2000/svg'>
-                <rect x="580" y="50" width="80" height="30" rx="15" className={indexStyles.stationCircleHeadingSvg} />
-                <text x="598" y="70.5" className={indexStyles.stationTextHeadingSvg}>InfoVis</text>
-                <polyline points="660,65 960,65 995,105 995,200 960,240 25,240 3,270 5,590" className={indexStyles.lineHeadingSvg} />
-              </svg>
-              <Image src={require('@/assets/train.svg')} alt="train" className={indexStyles.trainSvg} />
-              
-              <div className="column">
-                <Typography level="h1" className={indexStyles.titleHeading}>Visualizing the transportation <br /> shift in Germany</Typography>
-                <br />
-                <Typography level="h2" mt={2}>Comparing the usage of public transport and cars for <br /> the years from 2013 to 2022 across all federal states.</Typography>
-              </div>
-            </Stack>
-          </ChapterArea>
-        
+        <ChapterArea>
+          <Stack direction="column" className={indexStyles.headingStack} ref={introSectionRef}>
+            {/* SVG with line background */}
+            <svg className={indexStyles.headingSvg} viewBox="0 0 1000 200" version='1.1' xmlns='http://www.w3.org/2000/svg'>
+              <rect x="580" y="50" width="80" height="30" rx="15" className={indexStyles.stationCircleHeadingSvg} />
+              <text x="598" y="70.5" className={indexStyles.stationTextHeadingSvg}>InfoVis</text>
+              <polyline points="660,65 960,65 995,105 995,200 960,240 25,240 3,270 5,590" className={indexStyles.lineHeadingSvg} />
+            </svg>
+            <Image src={require('@/assets/train.svg')} alt="train" className={indexStyles.trainSvg} />
+
+            <div className="column">
+              <Typography level="h1" className={indexStyles.titleHeading}>Visualizing the transportation <br /> shift in Germany</Typography>
+              <br />
+              <Typography level="h2" mt={2}>Comparing the usage of public transport and cars for <br /> the years from 2013 to 2022 across all federal states.</Typography>
+            </div>
+          </Stack>
+        </ChapterArea>
+
 
         <ChapterArea>
           <Stack direction={"column"} id="project" mt={7} className={indexStyles.lineLeftStack} ref={projectSectionRef}>
@@ -87,8 +107,56 @@ const Home: React.FC<HomeProps> = ({ currentSection, setSection }) => {
         </ChapterArea>
 
         <ChapterArea>
+          <Stack direction={"column"} id="keyFinding" mt={7} className={indexStyles.lineLeftStack} ref={projectSectionRef}>
+            <Typography level="h2" className={currentSection == 2 ? indexStyles.markerLeftHeadingActive : indexStyles.markerLeftHeading}>Key Finding</Typography>
+            <Typography mt={2}>To continue select a specific key finding in order to take a deeper dive into our data!</Typography>
+            <Stack direction={"row"} mt={2} spacing={2}>
+              <Card sx={{ width: "30%" }}>
+                <CardCover>
+                  <TrendingUp sx={{ opacity: 0.1, maxWidth: "100px", position: 'relative', top: '-65px' }} />
+                </CardCover>
+                <CardContent>
+                  <Typography level="h3">Transportaion Shift</Typography>
+                  <Typography>See how transportation has evolved in resent years. Take a deeper look at how public transportaion and cars compete.</Typography>
+                  <div style={{ height: "100%" }}></div>
+                  <Button onClick={() => setCurrentKeyFinding(KeyFinding.Shift)} sx={{ width: "115px", mt: 2 }}>Take a look!</Button>
+                </CardContent>
+              </Card>
+              <Card sx={{ width: "30%" }}>
+                <CardCover>
+                  <Coronavirus sx={{ opacity: 0.1, maxWidth: "100px", position: 'relative', top: '-60px' }} />
+                </CardCover>
+                <CardContent>
+                  <Typography level="h3">COVID</Typography>
+                  <Typography>Due to the COVID-19 pandemic, the usage of public transportation has decreased significantly.</Typography>
+                  <div style={{ height: "100%" }}></div>
+                  <Button onClick={() => setCurrentKeyFinding(KeyFinding.Covid)} sx={{ width: "115px", mt: 2 }}>Take a look!</Button>
+                </CardContent>
+              </Card>
+              <Card sx={{ width: "30%" }}>
+                <CardCover>
+                  <DirectionsCar sx={{ opacity: 0.1, maxWidth: "100px", position: 'relative', top: '-60px' }} />
+                </CardCover>
+                <CardContent>
+                  <Typography level="h3">Cars in Germany</Typography>
+                  <Typography>
+                    Germany is known for its car industry. But how many cars are actually on the road? How many people own a car? How many people use public transportation?
+                  </Typography>
+                  <div style={{ height: "100%" }}></div>
+                  <Button sx={{ width: "115px", mt: 2 }}>Take a look!</Button>
+                </CardContent>
+              </Card>
+            </Stack>
+          </Stack>
+        </ChapterArea>
+
+        <ChapterArea>
+          {renderKeyFinding()}
+        </ChapterArea>
+
+        <ChapterArea>
           <Stack direction={"column"} id="insights" mt={7} className={indexStyles.lineLeftStack} ref={insightsSectionRef}>
-            <Typography level="h2" className={currentSection == 2 ? indexStyles.markerLeftHeadingActive : indexStyles.markerLeftHeading}>Insights and Map</Typography>
+            <Typography level="h2" className={currentSection == 3 ? indexStyles.markerLeftHeadingActive : indexStyles.markerLeftHeading}>Insights and Map</Typography>
             <Stack direction={"column"}>
               <Typography level="h3" mt={4}>Our Key Findings</Typography>
               <Stack
@@ -116,71 +184,71 @@ const Home: React.FC<HomeProps> = ({ currentSection, setSection }) => {
               <Comparison />
             </Stack>
             <Typography level="h3" mt={4} startDecorator={<Train />}>Insights: <i>&nbsp;Public Transportation</i></Typography>
-          <AccordionGroup size='lg' sx={{ my: 3, minWidth: "100%" }}>
-            <Accordion>
-              <AccordionSummary>Total PT Data in specific Year</AccordionSummary>
-              <AccordionDetails>
-                <Card sx={{ my: 3 }}><BarChartPT data={pTData} populationData={popData} /></Card>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion>
-              <AccordionSummary>Total PT Data development over Time</AccordionSummary>
-              <AccordionDetails>
-                <Card sx={{ my: 3 }}><LineChartPT data={pTData} /></Card>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion>
-              <AccordionSummary>Total PT Data change over Interval</AccordionSummary>
-              <AccordionDetails>
-                <Card sx={{ my: 3, minWidth: "100%" }}><BarChartPTDev data={pTData} /></Card>
-              </AccordionDetails>
-            </Accordion>
-          </AccordionGroup>
+            <AccordionGroup size='lg' sx={{ my: 3, minWidth: "100%" }}>
+              <Accordion>
+                <AccordionSummary>Total PT Data in specific Year</AccordionSummary>
+                <AccordionDetails>
+                  <Card sx={{ my: 3 }}><BarChartPT data={pTData} populationData={popData} /></Card>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion>
+                <AccordionSummary>Total PT Data development over Time</AccordionSummary>
+                <AccordionDetails>
+                  <Card sx={{ my: 3 }}><LineChartPT data={pTData} /></Card>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion>
+                <AccordionSummary>Total PT Data change over Interval</AccordionSummary>
+                <AccordionDetails>
+                  <Card sx={{ my: 3, minWidth: "100%" }}><BarChartPTDev data={pTData} /></Card>
+                </AccordionDetails>
+              </Accordion>
+            </AccordionGroup>
 
 
-          <Typography level="h3" mt={4} startDecorator={<DirectionsCar />}>Insights: <i>&nbsp;Cars</i></Typography>
-          <AccordionGroup size='lg' sx={{ my: 3, minWidth: "100%" }}>
-            <Accordion>
-              <AccordionSummary>Car Data in specific Year</AccordionSummary>
-              <AccordionDetails>
-                <Card sx={{ my: 3 }}><BarChartCar data={carData} populationData={popData} /></Card>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion>
-              <AccordionSummary>Car Data development over Time</AccordionSummary>
-              <AccordionDetails>
-              <Card sx={{ my: 3 }}><LineChartCar data={carData} /></Card>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion>
-              <AccordionSummary>Car Data change over Interval</AccordionSummary>
-              <AccordionDetails>
-              <Card sx={{ my: 3 }}><BarChartCarDev data={carData} /></Card>
-              </AccordionDetails>
-            </Accordion>
-          </AccordionGroup>
+            <Typography level="h3" mt={4} startDecorator={<DirectionsCar />}>Insights: <i>&nbsp;Cars</i></Typography>
+            <AccordionGroup size='lg' sx={{ my: 3, minWidth: "100%" }}>
+              <Accordion>
+                <AccordionSummary>Car Data in specific Year</AccordionSummary>
+                <AccordionDetails>
+                  <Card sx={{ my: 3 }}><BarChartCar data={carData} populationData={popData} /></Card>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion>
+                <AccordionSummary>Car Data development over Time</AccordionSummary>
+                <AccordionDetails>
+                  <Card sx={{ my: 3 }}><LineChartCar data={carData} /></Card>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion>
+                <AccordionSummary>Car Data change over Interval</AccordionSummary>
+                <AccordionDetails>
+                  <Card sx={{ my: 3 }}><BarChartCarDev data={carData} /></Card>
+                </AccordionDetails>
+              </Accordion>
+            </AccordionGroup>
 
-          <Typography level="h3" mt={4} startDecorator={<MergeType />}>Insights: <i>&nbsp;Combining Data Sets</i></Typography>
-          <AccordionGroup size='lg' sx={{ my: 3, minWidth: "100%" }}>
-            <Accordion>
-              <AccordionSummary>Combined Data development over Time</AccordionSummary>
-              <AccordionDetails>
-                <Card sx={{ my: 3 }}><BarChartDevelopmentCombined carData={carData} transportData={pTData}/></Card>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion>
-              <AccordionSummary>Combined Data in specific Year</AccordionSummary>
-              <AccordionDetails>
-                <Card sx={{ my: 3 }}><BarChartCombined carData={carData} transportData={pTData} populationData={popData} /></Card>
-              </AccordionDetails>
-            </Accordion>
-          </AccordionGroup>
+            <Typography level="h3" mt={4} startDecorator={<MergeType />}>Insights: <i>&nbsp;Combining Data Sets</i></Typography>
+            <AccordionGroup size='lg' sx={{ my: 3, minWidth: "100%" }}>
+              <Accordion>
+                <AccordionSummary>Combined Data development over Time</AccordionSummary>
+                <AccordionDetails>
+                  <Card sx={{ my: 3 }}><BarChartDevelopmentCombined carData={carData} transportData={pTData} /></Card>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion>
+                <AccordionSummary>Combined Data in specific Year</AccordionSummary>
+                <AccordionDetails>
+                  <Card sx={{ my: 3 }}><BarChartCombined carData={carData} transportData={pTData} populationData={popData} /></Card>
+                </AccordionDetails>
+              </Accordion>
+            </AccordionGroup>
           </Stack>
         </ChapterArea>
-        
+
         <ChapterArea>
           <Stack direction={"column"} mt={7} className={indexStyles.lineLeftHalfAcrossStack} ref={teamSectionRef}>
-            <Typography level="h2" id="team" className={currentSection == 3 ? indexStyles.markerLeftHeadingActive : indexStyles.markerLeftHeading}>Team</Typography>
+            <Typography level="h2" id="team" className={currentSection == 4 ? indexStyles.markerLeftHeadingActive : indexStyles.markerLeftHeading}>Team</Typography>
             <Stack mt={3} direction={"row"} alignItems={"center"} justifyContent={"center"} flexWrap="wrap" gap={15}>
               <TeamTile className={indexStyles.teamTileTop} imageSrc={require('@/assets/amiin.png')} name="Amiin Najjar" desc="HCI student with a passion for sport and cooking, I've swapped public transportation for pedaling my bike, blending tech insights with a dash of culinary creativity and a healthy dose of physical activity." />
               <TeamTile className={indexStyles.teamTileTop} imageSrc={require('@/assets/lukas.png')} name="Lukas Plenk" desc="I’m a Human-Computer-Interaction student at LMU Munich interested in digital media, culture, and traveling. Just like public transport, I’m always out for the next destination ahead." />
