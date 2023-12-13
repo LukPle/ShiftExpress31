a4bf1bf0bdec8d62db167e201696b0c719575b0c
MxnWgd <maxiw@outlook.com>
2023-12-13 11:08:41 +0100

    Fixed team tiles layout on front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import {
   AccordionDetails,
   AccordionSummary,
   Button,
+  Grid,
 } from "@mui/joy";
 import {
   Train,
@@ -342,6 +343,48 @@ const Home: React.FC<HomeProps> = ({ currentSection, setSection }) => {
             >
               Team
             </Typography>
+            <Grid container columns={6} rowSpacing={15} marginTop={-1}>
+              <Grid xs={2} display="flex" justifyContent="center" alignItems="center"> 
+                <TeamTile
+                  className={indexStyles.teamTileTop}
+                  imageSrc={"/amiin.png"}
+                  name="Amiin Najjar"
+                  desc="HCI student with a passion for sport and cooking, I've swapped public transportation for pedaling my bike, blending tech insights with a dash of culinary creativity and a healthy dose of physical activity."
+                />
+              </Grid>
+              <Grid xs={2} display="flex" justifyContent="center" alignItems="center">
+                <TeamTile
+                  className={indexStyles.teamTileTop}
+                  imageSrc={"/lukas.png"}
+                  name="Lukas Plenk"
+                  desc="I’m a Human-Computer-Interaction student at LMU Munich interested in digital media, culture, and traveling. Just like public transport, I’m always out for the next destination ahead."
+                />
+              </Grid>
+              <Grid xs={2} display="flex" justifyContent="center" alignItems="center">
+                <TeamTile
+                  className={indexStyles.teamTileTop}
+                  imageSrc={"/tim.png"}
+                  name="Timothy Summers"
+                  desc="I love collaborating in a team and solving creative challenges! Always ready for adventure - I can even handle Munich public transportation during rush hour!"
+                />
+              </Grid>
+              <Grid xs={3} display="flex" justifyContent="center" alignItems="center">
+                <TeamTile
+                  className={indexStyles.teamTileBottom}
+                  imageSrc={"/malek.png"}
+                  name="Malek Jarraya"
+                  desc="I’m a Media Informatics student at LMU Munich. I love colors, the sun, and the sea. I didn't know much about public transportation in the past, but our project definitely changed that."
+                />
+              </Grid>
+              <Grid xs={3} display="flex" justifyContent="center" alignItems="center">
+                <TeamTile
+                  className={indexStyles.teamTileBottom}
+                  imageSrc={"/maxi.png"}
+                  name="Maximilian Wiegand"
+                  desc="Hey, I'm a media computer science student at LMU Munich. I love to design, develop and explore - not only for computers. Some ideas even came up in delayed and overcrowded public transport…"
+                />
+              </Grid>
+            </Grid>
             <Stack
               mt={3}
               direction={"row"}
@@ -350,36 +393,6 @@ const Home: React.FC<HomeProps> = ({ currentSection, setSection }) => {
               flexWrap="wrap"
               gap={15}
             >
-              <TeamTile
-                className={indexStyles.teamTileTop}
-                imageSrc={"/amiin.png"}
-                name="Amiin Najjar"
-                desc="HCI student with a passion for sport and cooking, I've swapped public transportation for pedaling my bike, blending tech insights with a dash of culinary creativity and a healthy dose of physical activity."
-              />
-              <TeamTile
-                className={indexStyles.teamTileTop}
-                imageSrc={"/lukas.png"}
-                name="Lukas Plenk"
-                desc="I’m a Human-Computer-Interaction student at LMU Munich interested in digital media, culture, and traveling. Just like public transport, I’m always out for the next destination ahead."
-              />
-              <TeamTile
-                className={indexStyles.teamTileTop}
-                imageSrc={"/tim.png"}
-                name="Timothy Summers"
-                desc="I love collaborating in a team and solving creative challenges! Always ready for adventure - I can even handle Munich public transportation during rush hour!"
-              />
-              <TeamTile
-                className={indexStyles.teamTileBottom}
-                imageSrc={"/malek.png"}
-                name="Malek Jarraya"
-                desc="I’m a Media Informatics student at LMU Munich. I love colors, the sun, and the sea. I didn't know much about public transportation in the past, but our project definitely changed that."
-              />
-              <TeamTile
-                className={indexStyles.teamTileBottom}
-                imageSrc={"/maxi.png"}
-                name="Maximilian Wiegand"
-                desc="Hey, I'm a media computer science student at LMU Munich. I love to design, develop and explore - not only for computers. Some ideas even came up in delayed and overcrowded public transport…"
-              />
             </Stack>
           </Stack>
         </ChapterArea>
