22f166f4c9604da05dadd321ae8b8fca179f9cc5
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2024-01-02 11:14:26 +0100

    Added marker to timeline chart
    Also started setting up integration of tiemline chart to key finding

--- src/components/BaseCharts/BaseChartsSection.tsx
+++ src/components/BaseCharts/BaseChartsSection.tsx
@@ -3,6 +3,7 @@ import { Typography, Stack, Card, AccordionGroup, Accordion, AccordionSummary, A
 import { Train, DirectionsCar, MergeType } from "@mui/icons-material";
 import MapChart from '../MapComponents/Map';
 import TimeLineChart from './TimeLineChart';
+import LineChartTim from './LineChartTim';  
 import BarChartPT from './ChartsPT/BarChartPT';
 import BarChartPTDev from './ChartsPT/BarChartPTDevelopment';
 import BarChartCar from './ChartsCars/BarChartCar';
@@ -34,6 +35,7 @@ const BaseChartsSection: React.FC<BaseChartsSectionProps> = () => {
                 <Stack direction={"row"} mt={2}>
                   <MapChart />
                 </Stack>
+                <LineChartTim carData={carData} transportData={pTData} startYear='2013' endYear='2019' />
                 <TimeLineChart startYearProp="2013" endYearProp="2022" />
               </Stack>
             </AccordionDetails>
