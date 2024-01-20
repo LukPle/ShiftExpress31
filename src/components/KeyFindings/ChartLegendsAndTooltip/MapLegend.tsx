e958e7e1a9193fa14c34e4697b5b828f94e37645
Lukas Plenk <lukas.plenk@campus.lmu.de>
2024-01-20 12:02:23 +0100

    Refined map legend

--- src/components/KeyFindings/ChartLegendsAndTooltip/MapLegend.tsx
+++ src/components/KeyFindings/ChartLegendsAndTooltip/MapLegend.tsx
@@ -36,14 +36,16 @@ const MapLegend: React.FC<MapLegendProps> = ({ isPT, paddingEnd }) => {
 
     return (
         <Stack direction="column" maxWidth="100px" paddingBottom={paddingEnd + "px"}>
-            <Typography marginTop="15px" paddingBottom="15px">{isPT ? ptHeadline : carHeadline}</Typography>
-            <Stack direction="row">
+            <Typography marginTop="15px" paddingBottom="30px">{isPT ? ptHeadline : carHeadline}</Typography>
+            <Stack direction="row" alignItems={'center'}>
                 <Stack direction="column" justifyContent="space-evenly" alignItems="center" paddingRight="15px">
-                    <Typography>{isPT ? '+40' : '+10'}</Typography>
-                    <Typography>{isPT ? '+20' : '+5'}</Typography>
-                    <Typography>0</Typography>
-                    <Typography>{isPT ? '-20' : '-5'}</Typography>
-                    <Typography>{isPT ? '-40' : '-10'}</Typography>
+                    <Typography sx={{fontVariantNumeric: "tabular-nums"}}>^</Typography>
+                    <Typography sx={{fontVariantNumeric: "tabular-nums"}}>{isPT ? '+40' : '+10'}</Typography>
+                    <Typography sx={{fontVariantNumeric: "tabular-nums"}}>{isPT ? '+20' : '+5'}</Typography>
+                    <Typography sx={{fontVariantNumeric: "tabular-nums"}}>0</Typography>
+                    <Typography sx={{fontVariantNumeric: "tabular-nums"}}>{isPT ? '-20' : '-5'}</Typography>
+                    <Typography sx={{fontVariantNumeric: "tabular-nums"}}>{isPT ? '-40' : '-10'}</Typography>
+                    <Typography sx={{fontVariantNumeric: "tabular-nums"}}>⌄</Typography>
                 </Stack>
                 <InteractionTooltip tooltipText={`Color Scale for ${isPT ? 'Public Transport' : 'Cars'}`} delay={0} position={'bottom-start'}>
                     <Stack direction="column" divider={<Divider orientation="horizontal"/>} style={getColorColumnStyle}>
