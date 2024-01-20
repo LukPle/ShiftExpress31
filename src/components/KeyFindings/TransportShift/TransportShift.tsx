2d0d90f7acbc07c6a84769cb801d125dbb83a63d
Malek Jarraya <jarraya.malek@gmail.com>
2024-01-20 12:15:55 +0100

    slight modification of the focus text

--- src/components/KeyFindings/TransportShift/TransportShift.tsx
+++ src/components/KeyFindings/TransportShift/TransportShift.tsx
@@ -78,8 +78,8 @@ const TransportShift: React.FC = () => {
                   <Divider orientation="vertical" />
                   <Stack direction={"row"} spacing={2}>
                     <Button variant={currentFilter === FilterOptions.Comparison ? "solid" : "outlined"} onClick={() => setCurrentFilter(FilterOptions.Comparison)} sx={{ maxHeight: "30px" }}>🚊 vs 🚗 Comparison</Button>
-                    <Button variant={currentFilter === FilterOptions.FocusPublicTransport ? "solid" : "outlined"} onClick={() => setCurrentFilter(FilterOptions.FocusPublicTransport)} sx={{ maxHeight: "30px" }}>🚊 Focus Public Transport</Button>
-                    <Button variant={currentFilter === FilterOptions.FocusCars ? "solid" : "outlined"} onClick={() => setCurrentFilter(FilterOptions.FocusCars)} sx={{ maxHeight: "30px" }}>🚗 Focus Cars</Button>
+                    <Button variant={currentFilter === FilterOptions.FocusPublicTransport ? "solid" : "outlined"} onClick={() => setCurrentFilter(FilterOptions.FocusPublicTransport)} sx={{ maxHeight: "30px" }}>Focus: 🚊 Public Transport</Button>
+                    <Button variant={currentFilter === FilterOptions.FocusCars ? "solid" : "outlined"} onClick={() => setCurrentFilter(FilterOptions.FocusCars)} sx={{ maxHeight: "30px" }}>Focus: 🚗 Cars</Button>
                   </Stack>
                 </Stack>
               </CardContent>
